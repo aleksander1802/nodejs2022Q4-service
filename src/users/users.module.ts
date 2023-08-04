1e047fc369fb7491cb48c0ca121732773e232326
Alex2 <aleksander1802@gmail.com>
2023-08-04 18:13:15 +0300

    fix: fixed users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from 'nestjs-prisma';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
